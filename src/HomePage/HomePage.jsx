bd48dd505b377f06dd1df314a39032e08a918f15
Camilo Aguero <caguero@omnicomhealthgroup.com>
2019-10-23 10:49:38 -0500

    Add table and react bootstrap

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import { userService } from '../_services';
+import { Table } from 'react-bootstrap';
 
 class HomePage extends React.Component {
     constructor(props) {
@@ -23,23 +24,35 @@ class HomePage extends React.Component {
     };
 
     render() {
-        const { user, clients } = this.state;
-        console.log(clients);
+        const { user } = this.state;
+        let clientsRows;
+        if(this.state.clients.length>0){
+            clientsRows = this.state.clients.map((client)=> {
+                return (
+                    <tr key={client.Id}>
+                        <td>{client.Id}</td>
+                        <td>{client.Name}</td>
+                        <td>{client.LastName}</td>
+                    </tr>
+                )
+            });
+        }
         return (
             <div className="col-md-6 col-md-offset-3">
-                <h1>Hi {user.firstName}!</h1>
-                <p>You're logged in with React & Basic HTTP Authentication!!</p>
-                <h3>Users from secure api end point:</h3>
-                {/* {users.loading && <em>Loading users...</em>} */}
-                {clients.length &&
-                    <ul>
-                        {clients.map((client, Index) =>
-                            <li key={client.Id}>
-                                {client.Name + ' ' + client.LastName}
-                            </li>
-                        )}
-                    </ul>
-                }
+                <h1>Hi {user.Username}!</h1>
+                <h3>This is the list of insurances and clients:</h3>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Last Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {clientsRows}
+                    </tbody>
+                </Table>
                 <p>
                     <Link to="/login">Logout</Link>
                 </p>
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import { App } from './App';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 // setup fake backend
 // import { configureFakeBackend } from './_helpers';
